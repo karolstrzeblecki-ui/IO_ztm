96d0df6699c1c13270e4da2c2581171cd686c0a2
Jan Rodz <64307523+Nexisus@users.noreply.github.com>
2026-01-18 14:33:09 +0100

    Update test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -1,5 +1,7 @@
-from calc import add
-def test_add_working():
-    assert add(2,3) == 5
-def test_add_not_working():
-    assert add(2,3) == 6
+from calc import add   # albo: from app.calc import add
+
+def test_add_ok():
+    assert add(2, 3) == 5
+
+def test_add_fail():
+    assert add(2, 3) == 6
