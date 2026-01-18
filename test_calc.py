089e3bf7534b15f38249e797dabdef52cdce4f15
Jan Rodz <64307523+Nexisus@users.noreply.github.com>
2026-01-18 14:59:17 +0100

    Reformat test_calc.py for consistency

--- test_calc.py
+++ test_calc.py
@@ -4,6 +4,6 @@ from calc import add   # albo: from app.calc import add
 def test_add_ok():
     assert add(2, 3) == 5
 
-@pytest.mark.xfail(reason="Test celowo pokazuje błąd")
 def test_add_fail_example():
     assert add(2, 3) == 6
+
