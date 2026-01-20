1db89bfd641de87e2d8a12b3eed7f0fe5114c91e
karolstrzeblecki-ui <karol.strzeblecki@gmail.com>
2026-01-20 15:04:53 +0100

    test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -1,9 +1,5 @@
-import pytest
 from calc import add   # albo: from app.calc import add
+
 
 def test_add_ok():
     assert add(2, 3) == 5
-
-def test_add_fail_example():
-    assert add(2, 3) == 6
-
